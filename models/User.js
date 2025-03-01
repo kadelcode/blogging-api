01cd7f1364ace52a47d4b587e9e3f6c582dc764e
kadelcode <kadelcode@gmail.com>
2025-03-01 23:25:26 +0100

    User Schema

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true },
+}, { timestamps: true })
